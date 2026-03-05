ff29531db25a3dcf7d6eced030a9526f7f1e27d2
Shivanagesh123 <shivanageshboya@gmail.com>
2026-03-05 15:37:10 +0530

    the string is modified and another print statement is also added

--- src/test/java/map/Hello.java
+++ src/test/java/map/Hello.java
@@ -4,10 +4,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-  String str="Hello world";
+   String str="Hello GitHub";
    System.out.println(str.indexOf(str.substring(0,1)));
    System.out.println(str.lastIndexOf(str.substring(1,2)));
    System.out.println("the print statement is changed to track changes in gitHub");
+   System.out.println("The String is modified");
    }
 
 }
